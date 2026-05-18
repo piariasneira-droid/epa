b77390a6181e5c907314c907eb911a51df01f453
Pablo Arias <piariasneira@gmail.com>
2026-05-18 10:11:16 +0200

    modify gitignore

--- scr/update_tri/actualiza_microdatos_trimestre.r
+++ scr/update_tri/actualiza_microdatos_trimestre.r
@@ -1,12 +1,11 @@
-# Libraries ----
+# Environ ----
 source("./carga_librerias.R")
-
-# Trimestre de carga de microdatos
 trimestre_carga <- "2026T1"
 
-# Carga funciones para añadir trimestres
+# Load function
 source("./scr/read_data/anadir_csv_trimestre.R")
 source("./scr/read_data/read_data_funproc.R")
 
-# Actualizamos fichero de microdatos
+# Update microdata ----
 append_quarterly_data(trimestre_carga)
+
